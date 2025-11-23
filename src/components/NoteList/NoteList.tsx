b72c16dae0a4f66db919b5dccbda18585add7d81
Igor <ihor.kovalenko.01@gmail.com>
2025-11-23 21:13:18 +0100

    Created project

---
+++ src/components/NoteList/NoteList.tsx
@@ -0,0 +1,46 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import css from './NoteList.module.css';
+import type { Note } from '../../types/note';
+import { deleteNote } from '../../services/noteService';
+
+interface NotelistProps {
+  notes: Note[];
+}
+
+export default function NoteList({ notes }: NotelistProps) {
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: deleteNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['notes'] });
+    },
+  });
+
+  const handleDeleteNote = (id: string) => {
+    mutate(id);
+  };
+
+  return (
+    <ul className={css.list}>
+      {notes.map(note => {
+        return (
+          <li className={css.listItem} key={note.id}>
+            <h2 className={css.title}>{note.title}</h2>
+            <p className={css.content}>{note.content}</p>
+            <div className={css.footer}>
+              <span className={css.tag}>{note.tag}</span>
+              <button
+                type="button"
+                onClick={() => handleDeleteNote(note.id)}
+                className={css.button}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
---
+++ src/services/noteService.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+import type { Note } from '../types/note';
+
+interface fetchNotesProps {
+  search: string;
+  page: number;
+}
+
+export interface fetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+interface createNoteProps {
+  title: string;
+  content: string;
+  tag: string;
+}
+
+const myToken = import.meta.env.VITE_NOTEHUB_TOKEN;
+axios.defaults.baseURL = 'https://notehub-public.goit.study/api';
+axios.defaults.headers.common['Authorization'] = `Bearer ${myToken}`;
+
+export const fetchNotes = async ({
+  search,
+  page,
+}: fetchNotesProps): Promise<fetchNotesResponse> => {
+  const options = {
+    params: {
+      page,
+      perPage: 12,
+      search,
+    },
+  };
+
+  const response = await axios
+    .get('/notes', options)
+    .then(response => response.data);
+
+  // console.log('API response →', response);
+  return response;
+};
+
+export const createNote = async (data: createNoteProps) => {
+  const response = await axios.post<Note>('/notes', data, {});
+  // console.log(response.data);
+
+  return response.data;
+};
+
+export const deleteNote = async (id: Note['id']) => {
+  await axios.delete(`/notes/${id}`);
+};
